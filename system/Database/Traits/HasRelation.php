0855da655b1425973003a858a322b7e6d5259cc6
Yavar <mr_yavar@outlook.com>
2022-11-18 20:20:58 +0330

    Relations was added

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -4,19 +4,15 @@ namespace System\Database\Traits;
 
 trait HasRelation
 {
-
-
     protected function hasOne($model, $foreignKey, $localKey){
         if($this->{$this->primaryKey}){
-            $modelObject = new $model;
+            $modelObject = new $model();
             return $modelObject->getHasOneRelation($this->table, $foreignKey, $localKey, $this->$localKey);
         }
     }
 
     public function getHasOneRelation($table, $foreignKey, $otherKey, $otherKeyValue){
-
-        // sql = 'SELECT phones.* FROM users JOIN phones ON users.id = phones.user_id'
-        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+    $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
         $this->setWhere('AND', "`a`.`$otherKey` = ? ");
         $this->table = 'b';
         $this->addValue($otherKey, $otherKeyValue);
@@ -27,8 +23,54 @@ trait HasRelation
          return null;
     }
 
+    protected function hasMany($model, $foreignKey, $otherKey){
+        if($this->{$this->primaryKey}){
+            $modelObject = new $model;
+            return $modelObject->getHasManyRelation($this->table, $foreignKey, $otherKey, $this->$otherKey);
+        }
+    }
 
+    public function getHasManyRelation($table, $foreignKey, $otherKey, $otherKeyValue){
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere('AND', "`a`.`$otherKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($otherKey, $otherKeyValue);
+       return $this;
+    }
 
+    protected function belongsTo($model, $foreignKey, $localKey){
+        if($this->{$this->primaryKey}){
+            $modelObject = new $model();
+            return $modelObject->getBelongsToRelation($this->table, $foreignKey, $localKey, $this->$foreignKey);
+        }
+    }
 
+    public function getBelongsToRelation($table, $foreignKey, $otherKey, $foreignKeyValue){
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$foreignKey}` = `b`.`{$otherKey}` ");
+        $this->setWhere('AND', "`a`.`$foreignKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($foreignKey, $foreignKeyValue);
+        $statement = $this->executeQuery();
+        $data = $statement->fetch();
+        if ($data)
+        return $this->arrayToAttributes($data);
+         return null;
+    }
 
+    protected function belongsToMany($model, $commonTable, $localKey, $middleForeignKey, $middleRelation, $foreignKey )
+    {
+        if($this->{$this->primaryKey})
+        {
+            $modelObject = new $model();
+            return $modelObject->getBelongsToManyRelation($this->table, $commonTable , $localKey, $this->$localKey, $middleForeignKey, $middleRelation, $foreignKey);
+        }
+    }
+
+    protected function getBelongsToManyRelation($table, $commonTable, $localKey, $localKeyValue, $middleForeignKey, $middleRelation, $foreignKey)
+    {
+        $this->setSql("SELECT `c`.* FROM ( SELECT `b`.* FROM `{$table}` AS `a` JOIN `{$commonTable}` AS `b` on `a`.`{$localKey}` = `b`.`{$middleForeignKey}` WHERE  `a`.`{$localKey}` = ? ) AS `relation` JOIN ".$this->getTableName()." AS `c` ON `relation`.`{$middleRelation}` = `c`.`$foreignKey`");
+        $this->addValue("{$table}_{$localKey}", $localKeyValue);
+        $this->table = 'c';
+        return $this;
+    }
 }
